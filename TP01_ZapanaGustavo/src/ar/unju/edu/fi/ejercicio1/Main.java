f18a7e64da736fc1161ec76238201defa3085f6c
gmz78 <gmz78@gustavomz>
2024-04-04 16:46:30 -0300

    se agerga paquete y clase para el ejercicio 1

---
+++ TP01_ZapanaGustavo/src/ar/unju/edu/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.unju.edu.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+			int nu1 = 8;
+			int nu2 = 5;
+			int nu3 = 1;
+			int nu4 = 20;
+			int nu5 = 9;
+			
+			int promedio = (nu1+nu2+nu3+nu4+nu5) / 5;
+			
+			System.out.println(promedio);
+		}
+
+	}
+	
