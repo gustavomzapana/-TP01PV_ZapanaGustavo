c7c3f94f75bb6f6778980049c5f35809bd2389aa
gmz78 <gmz78@gustavomz>
2024-04-15 23:10:51 -0300

    Se agrega paquete del ejercicio 2

---
+++ TP01_ZapanaGustavo/src/ar/unju/edu/fi/ejercicio2/Main.java
@@ -0,0 +1,30 @@
+package ar.unju.edu.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombrePais;
+		int edad;
+		float alturaEdificio;
+		float precioProducto;
+		int numTelefono;
+		double coseno05;
+		
+		nombrePais = "Argentina";
+		edad = 31;
+		alturaEdificio = (float) 20.5;
+		precioProducto = (float) 50.99;
+		numTelefono = 4226699;
+		coseno05 = 0.87758256189037;
+		
+		System.out.println("Nombre Pais: "+nombrePais);
+		System.out.println(edad);
+		System.out.println(alturaEdificio);
+		System.out.println(precioProducto);
+		System.out.println(numTelefono);
+		System.out.println(coseno05);
+		
+	}
+
+}
