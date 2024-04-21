b11e57395ee6201dd055d19074782eb9519ef332
gmz78 <gmz78@gustavomz>
2024-04-20 23:11:23 -0300

    Se agrega paquete y clase del ejercicio 11.

---
+++ TP01_ZapanaGustavo/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //secuencia iterativa
+		int j;
+		int i = 2;
+		for(j=40;j>0;j--)
+		{  
+			 System.out.print(j*i +" ");
+			 i++;		
+		}
+	}
+
+}
